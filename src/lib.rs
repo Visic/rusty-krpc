6fa84988f3328770a5db5bfaa65f8ec216bb6574
Andrew <Andrew.Frailing@promessinc.com>
2022-05-31 22:25:52 -0400

    -Got the connection working

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 use std::error::Error;
-use prost::Message;
+use bytes::BytesMut;
+use prost::{Message, decode_length_delimiter};
+use prost::encoding::encoded_len_varint;
 use tokio::net::{TcpStream, ToSocketAddrs};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub mod api { include!(concat!(env!("OUT_DIR"), "/krpc.api.rs")); }
 
+#[derive(Debug)]
 pub struct Client {
-    buffer: Vec<u8>,
+    buffer: BytesMut,
     stream: TcpStream,
     identifier: Vec<u8>,
 }
@@ -15,13 +18,10 @@ impl Client {
     
 }
 
-//TODO:: This isn't quiet right, we need to send/receive the message length (and then remove read_to_eof)
-
 pub async fn connect_to(addr: impl ToSocketAddrs, client_name: impl Into<String>) -> Result<Client, Box<dyn Error>> {
     //basic objects we'll need
-    let i32_len = (0i32).encoded_len();
     let mut stream = TcpStream::connect(addr).await?;
-    let mut buffer = Vec::with_capacity(1024);
+    let mut buffer = BytesMut::with_capacity(1024);
 
     //make the connection request, encode it into the buffer, and send it to the server
     let request = api::ConnectionRequest{
@@ -29,23 +29,37 @@ pub async fn connect_to(addr: impl ToSocketAddrs, client_name: impl Into<String>
         r#type: api::connection_request::Type::Rpc as i32,
         client_identifier: Default::default()
     };
-    buffer.reserve(i32_len);
-    (request.encoded_len() as i32).encode(&mut buffer[0..i32_len])?;
-    request.encode(&mut buffer[i32_len..request.encoded_len()])?;
+    let request_len = request.encoded_len();
+    buffer.reserve(request_len + encoded_len_varint(request_len as u64));
+    request.encode_length_delimited(&mut buffer)?;
     stream.write_all(&buffer[..]).await?;
 
     //clear the buffer, read the server's response into it, and decode it as a connection response
-    buffer.clear();
-    buffer.reserve(i32_len);
-    stream.read_exact(&mut buffer[0..i32_len]).await?;
-    let message_len = i32::decode(&buffer[..])? as usize;
-    buffer.clear();
+    unsafe{ buffer.set_len(0); }
+    let message_len = loop {
+        buffer.reserve(buffer.len() + 1);
+        unsafe{ buffer.set_len(buffer.len() + 1); }
+        stream.read_exact(&mut buffer).await?;
+        if let Ok(v) = decode_length_delimiter(&mut buffer) { break v; }
+    };
     buffer.reserve(message_len);
-    stream.read_exact(&mut buffer[0..message_len]).await?;
+    unsafe{ buffer.set_len(message_len); }
+    stream.read_exact(&mut buffer).await?;
     let response = api::ConnectionResponse::decode(&buffer[..])?;
 
     //if the response is Ok, return our connected client, otherwise just return the error message
     (response.status == api::connection_response::Status::Ok as i32)
         .then_some(Client { buffer, stream, identifier: response.client_identifier })
         .ok_or_else(|| response.message.into())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::connect_to;
+
+    #[tokio::test]
+    async fn connection_test() {
+        let result = connect_to("127.0.0.1:50000", "HomeSweetHome").await;
+        println!("{:?}", result);
+    }
 }
